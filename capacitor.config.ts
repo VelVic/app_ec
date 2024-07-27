d2a3dfce9d4cbfef0690c7cc42ce8637a5986903
VictorMVH <20610323@utgz.edu.mnx>
2024-07-26 20:01:50 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eletric-cool',
+  webDir: 'www'
+};
+
+export default config;
